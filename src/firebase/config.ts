ea336285a7b95893126b36b76fbe04520b962d4f
Victor Herrera <vr.herrera.c@gmail.com>
2025-10-07 06:53:30 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-545694841-babc5",
+  "appId": "1:339636582859:web:f0217866d77a88c3d31e4e",
+  "apiKey": "AIzaSyB9NBDmi-EvS90tcZSedTWz82ST5SnsETE",
+  "authDomain": "studio-545694841-babc5.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "339636582859"
+};
