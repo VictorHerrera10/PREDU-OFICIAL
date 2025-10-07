9b9609b808d3af9b420239bebc00b60fe79e59ce
Victor Herrera <vr.herrera.c@gmail.com>
2025-10-07 07:05:30 +0000

    Try fixing this error: `Runtime Error: Error: Attempted to call the defa

--- src/app/dashboard/layout.tsx
+++ src/app/dashboard/layout.tsx
@@ -1,11 +1,27 @@
-import withAuth from "@/components/with-auth";
+'use client';
 
-function DashboardLayout({
-    children,
-  }: {
-    children: React.ReactNode;
-  }) {
-    return <>{children}</>;
+import { useUser } from '@/firebase/provider';
+import { redirect, usePathname } from 'next/navigation';
+import { useEffect } from 'react';
+
+export default function DashboardLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  const { user, isUserLoading } = useUser();
+  const pathname = usePathname();
+
+  useEffect(() => {
+    if (!isUserLoading && !user) {
+      redirect(`/login?redirect=${pathname}`);
+    }
+  }, [user, isUserLoading, pathname]);
+
+  if (isUserLoading || !user) {
+    // You can show a loading spinner here
+    return <div>Loading...</div>;
   }
-  
-  export default withAuth(DashboardLayout);
+
+  return <>{children}</>;
+}
