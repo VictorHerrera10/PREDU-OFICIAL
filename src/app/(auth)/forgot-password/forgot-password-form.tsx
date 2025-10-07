e6f58b1ea3d43264f224204732996e4a5a5cc936
Victor Herrera <vr.herrera.c@gmail.com>
2025-10-07 16:06:37 +0000

    starting build "30806fa1-0c5c-4df6-8813-c73955c1c09a" FETCHSOURCE BUILD

---
+++ src/app/(auth)/forgot-password/forgot-password-form.tsx
@@ -0,0 +1,74 @@
+'use client'
+
+import Link from 'next/link';
+import { useActionState, useEffect } from 'react';
+import { useSearchParams } from 'next/navigation';
+import { forgotPassword } from '@/app/actions';
+import { Input } from '@/components/ui/input';
+import { Label } from '@/components/ui/label';
+import { SubmitButton } from '@/components/submit-button';
+import { CardTitle, CardDescription, CardHeader } from '@/components/ui/card';
+import { ArrowLeft } from 'lucide-react';
+import { useToast } from '@/hooks/use-toast';
+
+const initialState = {
+  message: null,
+};
+
+export default function ForgotPasswordForm() {
+  const { toast } = useToast();
+  const [state, formAction] = useActionState(forgotPassword, initialState);
+  const searchParams = useSearchParams();
+  const formSubmitted = searchParams.get('message');
+
+  useEffect(() => {
+    if (state?.message) {
+      toast({
+        variant: 'destructive',
+        title: 'Ups... Algo salió mal 😵',
+        description: state.message,
+      });
+    }
+  }, [state, toast]);
+
+  useEffect(() => {
+    if (formSubmitted) {
+      toast({
+        title: '✉️ ¡Petición enviada!',
+        description: formSubmitted,
+      });
+    }
+  }, [formSubmitted, toast]);
+
+
+  return (
+    <>
+      <CardHeader className="p-0 mb-6 text-center">
+        <CardTitle className="text-2xl font-bold text-primary">🤔 ¿Olvidaste tu Contraseña?</CardTitle>
+        <CardDescription>¡Hasta al mejor estudiante le pasa! Te enviaremos instrucciones a tu correo para que puedas volver a clase. 🧑‍🏫</CardDescription>
+      </CardHeader>
+      
+      {formSubmitted ? (
+        <div className="text-center text-primary-foreground">
+            <p>{formSubmitted}</p>
+        </div>
+      ) : (
+        <form action={formAction} className="space-y-4">
+          <div className="space-y-2">
+            <Label htmlFor="email">✉️ Email</Label>
+            <Input id="email" name="email" type="email" placeholder="estudiante@email.com" required />
+          </div>
+          
+          <SubmitButton variant="secondary">Enviar Instrucciones 📬</SubmitButton>
+        </form>
+      )}
+
+      <div className="mt-6 text-center text-sm">
+        <Link href="/login" passHref className="font-semibold text-primary/80 hover:text-primary transition-colors flex items-center justify-center gap-2">
+          <ArrowLeft className="w-4 h-4" />
+          Volver a Iniciar Sesión
+        </Link>
+      </div>
+    </>
+  );
+}
