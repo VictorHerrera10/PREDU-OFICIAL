468b1e3e24cd8e4990853546bfed7437ffa52daa
Victor Herrera <vr.herrera.c@gmail.com>
2025-10-10 00:29:58 +0000

    el pacman se ve muy grande achicalo un poco

--- src/components/student-loader.tsx
+++ src/components/student-loader.tsx
@@ -10,7 +10,7 @@ type Props = {
 export function StudentLoader({ loadingText }: Props) {
     return (
         <div className="flex min-h-screen flex-col items-center justify-center bg-background p-4 gap-8">
-            <PacmanLoader color="#fde047" size={50} />
+            <PacmanLoader color="#fde047" size={35} />
             <p className="text-muted-foreground animate-pulse">{loadingText}</p>
        </div>
     );
